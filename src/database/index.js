ac4a3e292671c971b21b221f855125931b35e742
Igor Raphael <igorraphaeel@live.com>
2020-03-13 15:03:57 -0300

    map for model associate

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 import dbConfig from '../config/database';
 import User from '../app/models/User';
 import Client from '../app/models/Client';
+import Orderservice from '../app/models/Orderservice';
 
-const models = [User, Client];
+const models = [User, Client, Orderservice];
 
 class Database {
   constructor() {
@@ -12,7 +13,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(dbConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 export default new Database();
